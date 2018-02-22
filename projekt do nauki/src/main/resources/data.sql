69069a2b82226a58eb497f2e13c06480d5b511cb
Kamil Sałęga <kamilsalega@gmail.com>
2018-02-22 16:28:07 +0100

    Initcjalizacja bazy danych

---
+++ projekt do nauki/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (description) VALUES ('Amerykanska');
+INSERT INTO category (description) VALUES ('Wloska');
+INSERT INTO category (description) VALUES ('Polska');
+INSERT INTO category (description) VALUES ('Francuska');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Lyzeczka');
+INSERT INTO unit_of_measure (description) VALUES ('Lyzeczka');
+INSERT INTO unit_of_measure (description) VALUES ('Kubek');
+INSERT INTO unit_of_measure (description) VALUES ('Szczypta');
+INSERT INTO unit_of_measure (description) VALUES ('Uncja');
