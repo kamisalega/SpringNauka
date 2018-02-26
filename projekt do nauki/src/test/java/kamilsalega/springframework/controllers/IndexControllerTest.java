4c1cb1873c978285d9d13daf8e314fe28c18aa05
Kamil Sałęga <kamilsalega@gmail.com>
2018-02-26 04:24:35 +0100

    Błąd asercjii

--- projekt do nauki/src/test/java/kamilsalega/springframework/controllers/IndexControllerTest.java
+++ projekt do nauki/src/test/java/kamilsalega/springframework/controllers/IndexControllerTest.java
@@ -1,17 +1,23 @@
 package kamilsalega.springframework.controllers;
 
+import kamilsalega.springframework.domain.Recipe;
 import kamilsalega.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class IndexControllerTest {
     @Mock
@@ -32,10 +38,22 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() throws Exception {
 
+        Set<Recipe> recipes = new HashSet<>();
+
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = controller.getIndexPage(model);
 
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
