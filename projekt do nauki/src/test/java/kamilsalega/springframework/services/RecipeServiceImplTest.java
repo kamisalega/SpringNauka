03b10ff55eccf31e46363597290850eced5ca222
Kamil Sałęga <kamilsalega@gmail.com>
2018-02-26 03:48:20 +0100

    Pierwszy test przy pomocy mockito

---
+++ projekt do nauki/src/test/java/kamilsalega/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package kamilsalega.springframework.services;
+
+import kamilsalega.springframework.domain.Recipe;
+import kamilsalega.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet recipeSet = new HashSet<>();
+        recipeSet.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
